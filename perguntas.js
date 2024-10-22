8ebf1b20855b519cd10a89043dea7c43ce178edc
PamelaFernandes09 <140608243+PamelaFernandes09@users.noreply.github.com>
2024-10-22 08:54:35 -0300

    aula 22.10

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+craiCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguaguem de programação'
+)
+
+craiCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+craiCartao(
+    'Programação',
+    'O que é uma função',
+    resposta
+)
